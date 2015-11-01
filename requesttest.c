88229421ed5ee1a7b3acb73df77a8ba5bb6d14c1
sophiallen <smlelevich@gmail.com>
2015-10-31 22:44:47 -0700

    fixed seg fault issue, added more usage checks

--- requesttest.c
+++ requesttest.c
@@ -1,23 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <cs50.h>
 #include <strings.h>
 
 //Tests the use and implementation of strchr, strcpy, and strstr functions
 
 int main(void)
 {
-    char* line = "GET /cat.jpg HTTP/1.1\r\n";
+    char* line = GetString();
     printf("line: %s\n", line);
         
-    //right after method, before request: [3] 
+    //right after method, before request
     char* SP1 = strchr(line, ' ');
         
-    //after request, before HTTP [12]
+    //after request, before HTTP
     char* SP2 = strchr(SP1+1, ' ');
     
-    //after HTTP, start of CRLF [21]
+    //after HTTP, start of CRLF 
     char* CRLF = strstr(line, "\r\n");
+    if (CRLF == NULL)
+    {
+        printf("error 400: no crlf\n");
+    }
     
     //calculate length of sub-strings
     int methodsize = SP1-line; 
@@ -31,14 +36,14 @@ int main(void)
 
     //copy sub-strings to storage
     strncpy(method, line, methodsize);
-    strncpy(target, SP1+1, 8);
+    strncpy(target, SP1+1, targetsize);
     strncpy(http, SP2+1, httpsize);
     
     printf("method: %s\n", method);
     printf("target: %s\n", target);
     printf("http: %s\n", http);
     
-    if (strchr(target, ' ') != NULL)
+    if (strchr(method, ' ') != NULL || strchr(target, ' ') != NULL || strchr(http, ' ') != NULL)
     {
         printf("error 400: too many spaces\n");
     }
@@ -50,12 +55,28 @@ int main(void)
     }
     
     char* ext = strchr(target, '.');
+    if (ext == NULL || target[0] != '/')
+    {
+        printf("error 501: not implemented\n");
+    }
+    
     char* query = strchr(target, '?');
     if (query != NULL && query < ext)
     {
         printf("error 400: incorrect ? in target.\n");
     }
     
+    if(strchr(target, '"') != NULL)
+    {
+        printf("error 400: bad request by use of quotes\n");
+    }
+    
+    char* version = "HTTP/1.1";
+    if(strcmp(http, version)!=0)
+    {
+        printf("error 505: http version not supported\n");
+    }
+
     free(method);
     free(target);
     free(http);
